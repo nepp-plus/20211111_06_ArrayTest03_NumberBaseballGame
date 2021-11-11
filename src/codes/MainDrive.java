71615d310b2dd164378747ea7c793a8baa2dcf69
nepp-plus <nepp.plus@gmail.com>
2021-11-11 16:42:55 +0900

    cpu 문제 출제 - 맞는 숫자 나올때까지 계속 뽑는 구조

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -5,6 +5,28 @@ public class MainDrive {
 	public static void main(String[] args) {
 		
 		
+//		컴퓨터가 3자리 숫자 문제 출제.
+//		[7,4,1] 처럼 3칸의 배열로 만들자.
+		
+		int[]  cpuNumbers  = new int[3];
+		
+//		각 칸에 하나씩 (1~9) 랜덤 숫자를 채우자.
+		
+		for (int i=0 ; i < cpuNumbers.length ; i++) {
+			
+//			써도 되는 숫자가 나올때까지 무한 반복.
+			
+			while (true) {
+				
+				
+//				써도 되는 숫자면, cpuNumber의 위치에 맞게 집어넣고, 다음숫자로 넘어가자.
+				
+			}
+			
+			
+		}
+		
+		
 		
 	}
 
